1c7d5237abce2c677d25680e93aa9f38f9ed0538
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-24 20:28:22 +0300

    Add Flask protect processor function

--- python/flask-ctx/flask_ctx/context_processors.py
+++ python/flask-ctx/flask_ctx/context_processors.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from ctx_defense import CTX
+from flask import Markup
+from cgi import escape
 
 
 def ctx_processor():
     ctx_object = CTX()
+
+    def ctx_protect(secret, origin=None, alphabet=None):
+        protected_secret = ctx_object.protect(secret, origin, alphabet)
+        return Markup(
+            "<div data-ctx-origin='{origin_id}'>{permuted}</div>".format(
+                origin_id=protected_secret['origin_id'],
+                permuted=escape(protected_secret['permuted'])
+                )
+            )
+
     return {
+        'ctx_protect': ctx_protect,
     }
