e25ed67c67b0ebb9dfa6aa97784e2c0faad1a17a
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-24 20:16:28 +0300

    Add Django ctx protect tag

--- python/django-ctx/django_ctx/templatetags/ctx_tags.py
+++ python/django-ctx/django_ctx/templatetags/ctx_tags.py
@@ -3,3 +3,13 @@
 from django import template
 
 register = template.Library()
+
+
+@register.inclusion_tag('ctx/protected.html', takes_context=True)
+def ctx_protect(context, secret, origin=None, alphabet=None):
+    try:
+        ctx = context['ctx']
+    except KeyError:
+        raise KeyError('ctx not in Template context. Is the context processor for ctx properly set?')
+    protected_secret = ctx.protect(secret, origin, alphabet)
+    return protected_secret
