2794a742c458115767c15bdcb163ac1cc926fa31
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-24 20:16:44 +0300

    Add Django ctx permutations tag

--- python/django-ctx/django_ctx/templatetags/ctx_tags.py
+++ python/django-ctx/django_ctx/templatetags/ctx_tags.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django import template
+from json import dumps
 
 register = template.Library()
 
@@ -13,3 +14,16 @@ def ctx_protect(context, secret, origin=None, alphabet=None):
         raise KeyError('ctx not in Template context. Is the context processor for ctx properly set?')
     protected_secret = ctx.protect(secret, origin, alphabet)
     return protected_secret
+
+
+@register.inclusion_tag('ctx/permutations.html', takes_context=True)
+def ctx_permutations(context):
+    try:
+        ctx = context['ctx']
+    except KeyError:
+        raise KeyError('ctx not in Template context. Is the context processor for ctx properly set?')
+    return {
+        'permutations': dumps(
+            ctx.get_permutations()
+        )
+    }
