64a555b6caeb44a0e9ac22ad4bcba93e5f515ca9
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-28 16:43:00 +0300

    Test ascii printable permutation

--- python/ctx-defense/ctx_defense/tests/permuters_test.py
+++ python/ctx-defense/ctx_defense/tests/permuters_test.py
@@ -2,8 +2,13 @@
 
 import ctx_defense.permuters
 import unittest
+from string import printable
 
 
 class AsciiprintableTestCase(unittest.TestCase):
     def setUp(self):
         self.permuter = ctx_defense.permuters.AsciiPrintablePermuter()
+
+    def test_permutation(self):
+        permutation = self.permuter.get_permutation()
+        self.assertEqual(set(printable), set(permutation))
