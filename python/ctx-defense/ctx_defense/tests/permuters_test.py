8b8886ff1dc8e77a56e702b7a4411980c0b06ab9
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-28 16:43:35 +0300

    Test ascii printable different secret

--- python/ctx-defense/ctx_defense/tests/permuters_test.py
+++ python/ctx-defense/ctx_defense/tests/permuters_test.py
@@ -18,3 +18,10 @@ class AsciiprintableTestCase(unittest.TestCase):
         permuted_1 = self.permuter.permute(secret)
         permuted_2 = self.permuter.permute(secret)
         self.assertEqual(permuted_1, permuted_2)
+
+    def test_different_secret(self):
+        secret_1 = 'secret_1'
+        secret_2 = 'secret_2'
+        permuted_1 = self.permuter.permute(secret_1)
+        permuted_2 = self.permuter.permute(secret_2)
+        self.assertNotEqual(permuted_1, permuted_2)
