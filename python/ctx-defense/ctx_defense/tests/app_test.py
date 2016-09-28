9eb1fa4abcb1a476a54b9074e82d7b60747a2423
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-28 16:39:48 +0300

    Test permutation unavailable

--- python/ctx-defense/ctx_defense/tests/app_test.py
+++ python/ctx-defense/ctx_defense/tests/app_test.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import ctx_defense
+from ctx_defense.app import PermutationUnavailableError
 import unittest
 
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.ctx = ctx_defense.CTX()
+
+    def test_permutation_unavailable(self):
+        try:
+            self.ctx.protect('secret', 'origin', 'invalid alphabet')
+        except PermutationUnavailableError:
+            pass
