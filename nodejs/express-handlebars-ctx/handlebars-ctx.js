99b9f80c681f9d1ff8b7cf1b31d97e455d772beb
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-09-29 02:28:34 +0300

    Add handlebars-ctx helpers

---
+++ nodejs/express-handlebars-ctx/handlebars-ctx.js
@@ -0,0 +1,21 @@
+const ctx = require('../ctx'),
+      htmlescape = require('html-escape');
+
+module.exports = {
+    createCtxObject: function() {
+        
+        let ctxDefense = new ctx;
+        return {
+            ctxProtect: function(secret, origin) {
+                let protect = ctxDefense.protect(secret, origin); 
+                return '<div data-ctx-origin=' + protect.origin_id + '>' +
+                    htmlescape(protect.permuted) + '</div>';
+            },
+            ctxPermutations: function() {
+                let permutations = ctxDefense.getPermutations();
+                return '<script type="application/json" id="ctx-permutations">' +
+                    JSON.stringify(permutations) + '</script>';
+            }
+        };
+    }
+}
