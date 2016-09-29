818b31da89e2daa48dfbb89ed165eea0f2695560
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-29 21:37:30 +0300

    Add Django BaseTestCase

--- python/django-ctx/django_ctx/tests/context_processor_test.py
+++ python/django-ctx/django_ctx/tests/context_processor_test.py
@@ -3,6 +3,13 @@
 import os
 
 from django import setup
+from django.test import TestCase
+from django.test.client import RequestFactory
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ctx.tests.settings")
 setup()
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.reqfactory = RequestFactory()
