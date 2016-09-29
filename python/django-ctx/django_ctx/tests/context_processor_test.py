dc2a6e445f6962c623750447cfc9f58477a814d5
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-29 21:38:11 +0300

    Add Django test for ctx context variable injection

--- python/django-ctx/django_ctx/tests/context_processor_test.py
+++ python/django-ctx/django_ctx/tests/context_processor_test.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 
 from django import setup
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.template.response import TemplateResponse
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ctx.tests.settings")
 setup()
@@ -13,3 +15,14 @@ setup()
 class BaseTestCase(TestCase):
     def setUp(self):
         self.reqfactory = RequestFactory()
+
+
+class CtxprocessorTestCase(BaseTestCase):
+    def test_context_variable(self):
+        req = self.reqfactory.get('/')
+        res = TemplateResponse(req, 'variable.html', {})
+        res.render()
+        regex = r"&lt;ctx_defense\.app\.CTX object at 0x[0-9a-f]*&gt;"
+        self.assertTrue(
+            re.match(regex, res.rendered_content)
+        )
