2a54afb9ee19d296a6b3f719021f75168c32f4c6
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-30 12:31:53 +0300

    Test Flask protect tag

--- python/flask-ctx/flask_ctx/tests/context_processor_test.py
+++ python/flask-ctx/flask_ctx/tests/context_processor_test.py
@@ -2,8 +2,15 @@
 
 from unittest import TestCase
 from flask_ctx.context_processors import ctx_processor
+import re
+from string import printable
 
 
 class AsciiprintableTestCase(TestCase):
     def setUp(self):
         self.processor = ctx_processor()
+
+    def test_protect(self):
+        protected_tag = self.processor['ctx_protect']('secret', 'origin')
+        regex = r"[{p}]*(<div data-ctx-origin=')(\d*)('>)([{p}]*)(</div>)[{p}]*".format(p=re.escape(printable))
+        self.assertTrue(re.match(regex, protected_tag))
