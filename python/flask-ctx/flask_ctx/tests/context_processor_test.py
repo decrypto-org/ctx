abfde624cf2905cc62a030d14d9a98c2f5dec80e
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-09-30 12:32:07 +0300

    Test Flask empty permutations tag

--- python/flask-ctx/flask_ctx/tests/context_processor_test.py
+++ python/flask-ctx/flask_ctx/tests/context_processor_test.py
@@ -14,3 +14,8 @@ class AsciiprintableTestCase(TestCase):
         protected_tag = self.processor['ctx_protect']('secret', 'origin')
         regex = r"[{p}]*(<div data-ctx-origin=')(\d*)('>)([{p}]*)(</div>)[{p}]*".format(p=re.escape(printable))
         self.assertTrue(re.match(regex, protected_tag))
+
+    def test_empty_permutations(self):
+        regex = r"[{p}]*(<script type='application\/json' id='ctx-permutations'>\[\]<\/script>)[{p}]*".format(p=re.escape(printable))
+        permutations_tag = self.processor['ctx_permutations']()
+        self.assertTrue(re.match(regex, permutations_tag))
